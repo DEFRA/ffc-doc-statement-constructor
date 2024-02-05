69396b45cf47391ab4b5325e0f5310a70b4e8b71
Abidemi Adio <abidemi.adio@defra.gov.uk>
2024-02-05 00:11:08 +0000

    178681 Update ffc-doc-statement-constructor to include all recent changes made in ffc-pay-statement-constructor

--- app/processing/components/get-schedule-dates.js
+++ app/processing/components/get-schedule-dates.js
@@ -1,4 +1,6 @@
+const moment = require('moment')
 const { convertToPounds } = require('../../utility')
+const { IMMEDIATE, QUARTERLY } = require('../../../app/constants/payment-type')
 
 const getScheduleDates = (previousPaymentSchedule, newPaymentSchedule, deltaValue) => {
   if (previousPaymentSchedule.every(x => x.outstanding)) {
@@ -7,27 +9,29 @@ const getScheduleDates = (previousPaymentSchedule, newPaymentSchedule, deltaValu
 
   const paidSegments = previousPaymentSchedule.filter(x => !x.outstanding)
   newPaymentSchedule.splice(0, paidSegments.length)
-  if (deltaValue < 0) {
+  const nonAdjustmentValue = 0
+
+  if (deltaValue < nonAdjustmentValue) {
     // we need to avoid a balloon reduction, so we spread the reduction for any paid segments across remaining segments only
     const correctionValue = ((Math.abs(deltaValue) / previousPaymentSchedule.length) * paidSegments.length) / newPaymentSchedule.length
     newPaymentSchedule.forEach(x => { x.value = x.value - correctionValue < 0 ? 0 : x.value - correctionValue })
   }
-  if (deltaValue > 0) {
-    paidSegments.push({
-      period: 'Adjustment',
+  if (deltaValue > nonAdjustmentValue) {
+    newPaymentSchedule.unshift({
+      paymentType: IMMEDIATE,
+      period: moment().format('MMMM YYYY'),
       value: Math.trunc((deltaValue / previousPaymentSchedule.length) * paidSegments.length)
     })
   }
 
-  const schedule = paidSegments.concat(newPaymentSchedule)
-
-  return mapSchedule(schedule)
+  return mapSchedule(newPaymentSchedule)
 }
 
 const mapSchedule = (schedule) => {
   return schedule.map((segment, i) => ({
     order: i + 1,
     dueDate: segment.dueDate?.format('DD/MM/YYYY'),
+    paymentType: segment.paymentType ?? QUARTERLY,
     period: segment.period,
     value: convertToPounds(segment.value)
   }))
--- test/unit/processing/components/get-schedule-dates.test.js
+++ test/unit/processing/components/get-schedule-dates.test.js
@@ -1,5 +1,7 @@
 const moment = require('moment')
 const getScheduleDates = require('../../../../app/processing/components/get-schedule-dates')
+const { IMMEDIATE, QUARTERLY } = require('../../../../app/constants/payment-type')
+
 let previousPaymentSchedule
 let newPaymentScheduleTopUp
 let newPaymentScheduleReduction
@@ -10,66 +12,66 @@ describe('get schedule dates', () => {
     previousPaymentSchedule = [{
       dueDate: moment('2021-01-01'),
       outstanding: false,
-      period: 'September to December 2020',
+      period: 'Sep-Dec 2020',
       value: 25000
     }, {
       dueDate: moment('2021-04-01'),
       outstanding: false,
-      period: 'January to March 2021',
+      period: 'Jan-Mar 2021',
       value: 25000
     }, {
       dueDate: moment('2021-07-01'),
       outstanding: true,
-      period: 'April to June 2021',
+      period: 'Apr-Jun 2021',
       value: 25000
     }, {
       dueDate: moment('2021-10-01'),
       outstanding: true,
-      period: 'July to September 2021',
+      period: 'Jul-Sep 2021',
       value: 25000
     }]
 
     newPaymentScheduleTopUp = [{
       dueDate: moment('2021-01-01'),
       outstanding: false,
-      period: 'September to December 2020',
+      period: 'Sep-Dec 2020',
       value: 50000
     }, {
       dueDate: moment('2021-04-01'),
       outstanding: true,
-      period: 'January to March 2021',
+      period: 'Jan-Mar 2021',
       value: 50000
     }, {
       dueDate: moment('2021-07-01'),
       outstanding: true,
-      period: 'April to June 2021',
+      period: 'Apr-Jun 2021',
       value: 50000
     }, {
       dueDate: moment('2021-10-01'),
       outstanding: true,
-      period: 'July to September 2021',
+      period: 'Jul-Sep 2021',
       value: 50000
     }]
 
     newPaymentScheduleReduction = [{
       dueDate: moment('2021-01-01'),
       outstanding: false,
-      period: 'September to December 2020',
+      period: 'Sep-Dec 2020',
       value: 12500
     }, {
       dueDate: moment('2021-04-01'),
       outstanding: true,
-      period: 'January to March 2021',
+      period: 'Jan-Mar 2021',
       value: 12500
     }, {
       dueDate: moment('2021-07-01'),
       outstanding: true,
-      period: 'April to June 2021',
+      period: 'Apr-Jun 2021',
       value: 12500
     }, {
       dueDate: moment('2021-10-01'),
       outstanding: true,
-      period: 'July to September 2021',
+      period: 'Jul-Sep 2021',
       value: 12500
     }]
 
@@ -93,68 +95,69 @@ describe('get schedule dates', () => {
 
   test('should insert top up adjustment after last outstanding payment in original schedule', () => {
     const schedule = getScheduleDates(previousPaymentSchedule, newPaymentScheduleTopUp, deltaValue)
-    expect(schedule[2].period).toEqual('Adjustment')
+    expect(schedule[0].period).toEqual(moment().format('MMMM YYYY'))
   })
 
-  test('should retain previous payment value for paid segments if top up', () => {
+  test('Adjustment payment Type must be Immediate payment', () => {
     const schedule = getScheduleDates(previousPaymentSchedule, newPaymentScheduleTopUp, deltaValue)
-    expect(schedule[0].value).toEqual('250.00')
-    expect(schedule[1].value).toEqual('250.00')
+    expect(schedule[0].paymentType).toEqual(IMMEDIATE)
+    expect(schedule[0].paymentType).not.toEqual(QUARTERLY)
+  })
+
+  test('should not retain previous payment value for paid segments if top up', () => {
+    const schedule = getScheduleDates(previousPaymentSchedule, newPaymentScheduleTopUp, deltaValue)
+    expect(schedule[0].paymentType).toEqual(IMMEDIATE)
   })
 
   test('should calculate new payment value for unpaid segments if top up', () => {
     const schedule = getScheduleDates(previousPaymentSchedule, newPaymentScheduleTopUp, deltaValue)
-    expect(schedule[3].value).toEqual('500.00')
-    expect(schedule[4].value).toEqual('500.00')
+    expect(schedule[1].value).toEqual('500.00')
+    expect(schedule[2].value).toEqual('500.00')
+  })
+
+  test('Non-Adjustment payment Type must be Quarterly Payment', () => {
+    const schedule = getScheduleDates(previousPaymentSchedule, newPaymentScheduleTopUp, deltaValue)
+    expect(schedule[1].paymentType).toEqual(QUARTERLY)
+    expect(schedule[2].paymentType).toEqual(QUARTERLY)
   })
 
   test('should calculate top up value for adjustment', () => {
     const schedule = getScheduleDates(previousPaymentSchedule, newPaymentScheduleTopUp, deltaValue)
-    expect(schedule[2].value).toEqual('250.00')
+    expect(schedule[0].value).toEqual('250.00')
   })
 
-  test('should insert top up as last scheduled item no remaining payments', () => {
+  test('should insert top up as first and only scheduled item when no remaining payments are to be made', () => {
     previousPaymentSchedule.forEach(x => { x.outstanding = false })
     const schedule = getScheduleDates(previousPaymentSchedule, newPaymentScheduleReduction, deltaValue)
-    expect(schedule[0].value).toEqual('250.00')
-    expect(schedule[1].value).toEqual('250.00')
-    expect(schedule[2].value).toEqual('250.00')
-    expect(schedule[3].value).toEqual('250.00')
-    expect(schedule[4].value).toEqual('500.00')
+    expect(schedule[0].value).toEqual('500.00')
+    expect(schedule.length).toBe(1)
   })
 
   test('should not insert adjustment into schedule if reduction', () => {
     deltaValue = -25000
     const schedule = getScheduleDates(previousPaymentSchedule, newPaymentScheduleReduction, deltaValue)
-    expect(schedule[2].period).not.toEqual('Adjustment')
-    expect(schedule.length).toEqual(previousPaymentSchedule.length)
-  })
-
-  test('should retain previous payment value for paid segments if reduction', () => {
-    deltaValue = -25000
-    const schedule = getScheduleDates(previousPaymentSchedule, newPaymentScheduleReduction, deltaValue)
-    expect(schedule[0].value).toEqual('250.00')
-    expect(schedule[1].value).toEqual('250.00')
+    expect(schedule[0].period).not.toEqual(moment().format('MMMM YYYY'))
+    expect(schedule.length).toEqual(newPaymentScheduleReduction.length)
   })
 
   test('should calculate new payment value for unpaid segments if reduction', () => {
     deltaValue = -25000
     const schedule = getScheduleDates(previousPaymentSchedule, newPaymentScheduleReduction, deltaValue)
-    expect(schedule[2].value).toEqual('62.50')
-    expect(schedule[3].value).toEqual('62.50')
+    expect(schedule[0].value).toEqual('62.50')
+    expect(schedule[1].value).toEqual('62.50')
   })
 
   test('should calculate new payment value for unpaid segments if reduction results in zero remaining payments', () => {
     deltaValue = -50000
     const schedule = getScheduleDates(previousPaymentSchedule, newPaymentScheduleReduction, deltaValue)
-    expect(schedule[2].value).toEqual('0.00')
-    expect(schedule[3].value).toEqual('0.00')
+    expect(schedule[0].value).toEqual('0.00')
+    expect(schedule[1].value).toEqual('0.00')
   })
 
   test('should not display negative schedule values if recovery', () => {
     deltaValue = -100000
     const schedule = getScheduleDates(previousPaymentSchedule, newPaymentScheduleReduction, deltaValue)
-    expect(schedule[2].value).toEqual('0.00')
-    expect(schedule[3].value).toEqual('0.00')
+    expect(schedule[0].value).toEqual('0.00')
+    expect(schedule[1].value).toEqual('0.00')
   })
 })
