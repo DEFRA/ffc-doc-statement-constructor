08e87f68ac92c0f66dcba442fce1b4eebd558f8d
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2024-03-13 14:02:43 +0000

    cleaned up dax tests

--- test/mock-objects/mock-dax.js
+++ test/mock-objects/mock-dax.js
@@ -1,7 +1,9 @@
+const { DAX: DAX_TYPE } = require('../../app/constants/types')
 const { DATE: UPDATED_TIMESTAMP } = require('../mock-components/mock-dates').UPDATED
 
 module.exports = {
   calculationId: 12345678901,
   transactionDate: UPDATED_TIMESTAMP,
-  datePublished: UPDATED_TIMESTAMP
+  datePublished: UPDATED_TIMESTAMP,
+  type: DAX_TYPE
 }
