e64bc3f0fc61c0d190b1db9f0fbd93f622f81788
Abidemi Adio <abidemi.adio@defra.gov.uk>
2024-04-01 23:23:13 +0100

    Resolved SonarCloud issues

--- app/processing/organisation/schema.js
+++ app/processing/organisation/schema.js
@@ -1,19 +1,14 @@
 const Joi = require('joi')
 
-const minSbi = 105000000
-const maxSbi = 999999999
-const minFrn = 1000000000
-const maxFrn = 9999999999
-
 module.exports = Joi.object({
-  sbi: Joi.number().integer().min(minSbi).max(maxSbi).required(),
+  sbi: Joi.number().integer().min(105000000).max(999999999).required(),
   addressLine1: Joi.string().allow('', null).optional(),
   addressLine2: Joi.string().allow('', null).optional(),
   addressLine3: Joi.string().allow('', null).optional(),
   city: Joi.string().allow('', null).optional(),
   county: Joi.string().allow('', null).optional(),
   emailAddress: Joi.string().allow('', null).optional(),
-  frn: Joi.number().integer().min(minFrn).max(maxFrn).required(),
+  frn: Joi.number().integer().min(1000000000).max(9999999999).required(),
   name: Joi.string().required(),
   postcode: Joi.string().allow('', null).optional()
 }).required()
