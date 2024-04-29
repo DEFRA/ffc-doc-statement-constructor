aecb12516305f81883a90296daec82b1f47f0775
AbidemiAdio <98023713+AbidemiAdio@users.noreply.github.com>
2024-04-29 21:31:34 +0100

    Removed null constraint on paymentperiod (#28)
    * Removed null constraint on paymentperiod
    
    * Updated version number

--- app/inbound/dax/schema.js
+++ app/inbound/dax/schema.js
@@ -7,7 +7,7 @@ const number200 = 200
 module.exports = Joi.object({
   paymentReference: Joi.string().max(number30).required(),
   calculationReference: Joi.number().integer().allow(null),
-  paymentPeriod: Joi.string().max(number200).required(),
+  paymentPeriod: Joi.string().max(number200).allow('', null).optional(),
   paymentAmount: Joi.number().required(),
   transactionDate: Joi.date().required(),
   datePublished: Joi.date().allow(null),
