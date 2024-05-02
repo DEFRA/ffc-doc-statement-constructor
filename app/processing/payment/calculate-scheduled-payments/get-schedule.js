2c0804ce8a3103a36005fb73604bde85bc8ce070
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2024-05-02 12:03:16 +0100

    reinstate incorrectly overwritten file

--- app/processing/payment/calculate-scheduled-payments/get-schedule.js
+++ app/processing/payment/calculate-scheduled-payments/get-schedule.js
@@ -1,23 +1,23 @@
-const getPeriodFromSchedule = require('./get-period-from-schedule')
+const getExpectedSettlementValue = require('./get-expected-settlement-value')
+const getExpectedValue = require('./get-expected-value')
+const getPeriod = require('./get-period')
 
-const getSchedule = ({ scheduleDate, totalPayments, increment, unit, settlementDate }, { paymentValue, settledValue, previousSettledValue, totalValue }) => {
+const getSchedule = (scheduleDate, totalPayments, settledValue, totalValue, increment, unit, currentDate) => {
   const scheduleDates = []
-  let expectedSettlementValue = 0
   for (let i = 1; i <= totalPayments; i++) {
-    expectedSettlementValue = getExpectedValue(totalValue, totalPayments, i)
+    const expectedValue = getExpectedValue(totalValue, totalPayments, i === totalPayments)
+    const expectedSettlementValue = getExpectedSettlementValue(totalValue, totalPayments, i)
     const cappedSettlementValue = settledValue <= expectedSettlementValue ? settledValue : expectedSettlementValue
     scheduleDates.push({
       dueDate: scheduleDate.clone(),
-      outstanding: cappedSettlementValue > previousSettledValue && cappedSettlementValue <= settledValue
+      period: getPeriod(scheduleDate, increment, unit),
+      value: expectedValue,
+      outstanding: scheduleDate >= currentDate || cappedSettlementValue < expectedSettlementValue
     })
     scheduleDate = scheduleDate.add(increment, unit)
   }
 
-  return getPeriodFromSchedule(totalValue, totalPayments, paymentValue, scheduleDates, increment, unit, settlementDate)
-}
-
-const getExpectedValue = (totalValue, totalPayments, segment) => {
-  return Math.trunc(totalValue / totalPayments * segment)
+  return scheduleDates
 }
 
 module.exports = getSchedule
