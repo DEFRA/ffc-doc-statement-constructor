80ebfd697ff97f0794556af046d81e84353fd8ce
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2024-08-02 15:17:01 +0100

    tweaking tests to remove duplication flags

--- app/data/models/delinkedCalculation.js
+++ app/data/models/delinkedCalculation.js
@@ -1,36 +1,33 @@
 module.exports = (sequelize, DataTypes) => {
-  const number0 = 0
-  const number11 = 11
+  const scale0 = 0
+  const precision11 = 11
   const maxFRN = 16
   const maxSBI = 38
+
+  const commonStringField = { type: DataTypes.STRING, allowNull: false }
+
   const delinkedCalculation = sequelize.define('delinkedCalculation', {
-    applicationId: { type: DataTypes.NUMBER(number11, number0), allowNull: false },
-    calculationId: { type: DataTypes.NUMBER(number11, number0), primaryKey: true, allowNull: false },
-    sbi: { type: DataTypes.NUMBER(maxSBI, number0), allowNull: false },
+    applicationId: { type: DataTypes.NUMBER(precision11, scale0), allowNull: false },
+    calculationId: { type: DataTypes.NUMBER(precision11, scale0), primaryKey: true, allowNull: false },
+    sbi: { type: DataTypes.NUMBER(maxSBI, scale0), allowNull: false },
     frn: { type: DataTypes.STRING(maxFRN), allowNull: false },
-    paymentBand1: { type: DataTypes.STRING, allowNull: false },
-    paymentBand2: { type: DataTypes.STRING, allowNull: false },
-    paymentBand3: { type: DataTypes.STRING, allowNull: false },
-    paymentBand4: { type: DataTypes.STRING, allowNull: false },
-    percentageReduction1: { type: DataTypes.STRING, allowNull: false },
-    percentageReduction2: { type: DataTypes.STRING, allowNull: false },
-    percentageReduction3: { type: DataTypes.STRING, allowNull: false },
-    percentageReduction4: { type: DataTypes.STRING, allowNull: false },
-    progressiveReductions1: { type: DataTypes.STRING, allowNull: false },
-    progressiveReductions2: { type: DataTypes.STRING, allowNull: false },
-    progressiveReductions3: { type: DataTypes.STRING, allowNull: false },
-    progressiveReductions4: { type: DataTypes.STRING, allowNull: false },
-    referenceAmount: { type: DataTypes.STRING, allowNull: false },
-    totalProgressiveReduction: { type: DataTypes.STRING, allowNull: false },
-    totalDelinkedPayment: { type: DataTypes.STRING, allowNull: false },
-    paymentAmountCalculated: { type: DataTypes.STRING, allowNull: false },
-    datePublished: { type: DataTypes.DATE, allowNull: true }
+    ...Array.from({ length: 4 }, (_, i) => ({
+      [`paymentBand${i + 1}`]: commonStringField,
+      [`percentageReduction${i + 1}`]: commonStringField,
+      [`progressiveReductions${i + 1}`]: commonStringField
+    })).reduce((acc, curr) => ({ ...acc, ...curr }), {}),
+    referenceAmount: commonStringField,
+    totalProgressiveReduction: commonStringField,
+    totalDelinkedPayment: commonStringField,
+    paymentAmountCalculated: commonStringField,
+    datePublished: { type: DataTypes.DATE, allowNull: false }
   },
   {
     tableName: 'delinkedCalculation',
     freezeTableName: true,
     timestamps: false
   })
+
   delinkedCalculation.associate = function (models) {
     delinkedCalculation.hasMany(models.dax, {
       foreignKey: 'calculationId',
@@ -41,5 +38,6 @@ module.exports = (sequelize, DataTypes) => {
       as: 'organisations'
     })
   }
+
   return delinkedCalculation
 }
--- app/inbound/delinked/schema.js
+++ app/inbound/delinked/schema.js
@@ -1,5 +1,30 @@
 const { Joi, constants, numberSchema } = require('../../utility/common-schema-fields')
 
+const createStringField = (fieldName) => Joi.string().required().messages({
+  'string.base': `${fieldName} should be a type of string`,
+  'any.required': `The field ${fieldName} is not present but it is required`
+})
+
+const createNumberField = (fieldName, min, max) => Joi.number().integer().min(min).max(max).required().messages({
+  'number.base': `${fieldName} should be a type of number`,
+  'number.integer': `${fieldName} should be an integer`,
+  'number.min': `${fieldName} should have a minimum value of ${min}`,
+  'number.max': `${fieldName} should have a maximum value of ${max}`,
+  'any.required': `The field ${fieldName} is not present but it is required`
+})
+
+const paymentBands = Array.from({ length: 4 }, (_, i) => ({
+  [`paymentBand${i + 1}`]: createStringField(`paymentBand${i + 1}`)
+})).reduce((acc, curr) => ({ ...acc, ...curr }), {})
+
+const percentageReductions = Array.from({ length: 4 }, (_, i) => ({
+  [`percentageReduction${i + 1}`]: createStringField(`percentageReduction${i + 1}`)
+})).reduce((acc, curr) => ({ ...acc, ...curr }), {})
+
+const progressiveReductions = Array.from({ length: 4 }, (_, i) => ({
+  [`progressiveReductions${i + 1}`]: createStringField(`progressiveReductions${i + 1}`)
+})).reduce((acc, curr) => ({ ...acc, ...curr }), {})
+
 module.exports = Joi.object({
   applicationReference: Joi.number().integer().required().messages({
     'number.base': 'applicationId should be a type of number',
@@ -7,84 +32,15 @@ module.exports = Joi.object({
     'any.required': 'The field applicationId is not present but it is required'
   }),
   calculationReference: numberSchema('calculationReference'),
-  sbi: Joi.number().integer().min(constants.minSbi).max(constants.maxSbi).required().messages({
-    'number.base': 'sbi should be a type of number',
-    'number.integer': 'sbi should be an integer',
-    'number.min': `sbi should have a minimum value of ${constants.minSbi}`,
-    'number.max': `sbi should have a maximum value of ${constants.maxSbi}`,
-    'any.required': 'The field sbi is not present but it is required'
-  }),
-  frn: Joi.number().integer().min(constants.minFrn).max(constants.maxFrn).required().messages({
-    'number.base': 'frn should be a type of number',
-    'number.integer': 'frn should be an integer',
-    'number.min': `frn should have a minimum value of ${constants.minFrn}`,
-    'number.max': `frn should have a maximum value of ${constants.maxFrn}`,
-    'any.required': 'The field frn is not present but it is required'
-  }),
-  paymentBand1: Joi.string().required().messages({
-    'string.base': 'paymentBand1 should be a type of string',
-    'any.required': 'The field paymentBand1 is not present but it is required'
-  }),
-  paymentBand2: Joi.string().required().messages({
-    'string.base': 'paymentBand2 should be a type of string',
-    'any.required': 'The field paymentBand2 is not present but it is required'
-  }),
-  paymentBand3: Joi.string().required().messages({
-    'string.base': 'paymentBand2 should be a type of string',
-    'any.required': 'The field paymentBand3 is not present but it is required'
-  }),
-  paymentBand4: Joi.string().required().messages({
-    'string.base': 'paymentBand4 should be a type of string',
-    'any.required': 'The field paymentBand4 is not present but it is required'
-  }),
-  percentageReduction1: Joi.string().required().messages({
-    'string.base': 'percentageReduction1 should be a type of string',
-    'any.required': 'The field percentageReduction1 is not present but it is required'
-  }),
-  percentageReduction2: Joi.string().required().messages({
-    'string.base': 'percentageReduction2 should be a type of string',
-    'any.required': 'The field percentageReduction2 is not present but it is required'
-  }),
-  percentageReduction3: Joi.string().required().messages({
-    'string.base': 'percentageReduction3 should be a type of string',
-    'any.required': 'The field percentageReduction3 is not present but it is required'
-  }),
-  percentageReduction4: Joi.string().required().messages({
-    'string.base': 'percentageReduction4 should be a type of string',
-    'any.required': 'The field percentageReduction4 is not present but it is required'
-  }),
-  progressiveReductions1: Joi.string().required().messages({
-    'string.base': 'progressiveReductions1 should be a type of string',
-    'any.required': 'The field progressiveReductions1 is not present but it is required'
-  }),
-  progressiveReductions2: Joi.string().required().messages({
-    'string.base': 'progressiveReductions2 should be a type of string',
-    'any.required': 'The field progressiveReductions2 is not present but it is required'
-  }),
-  progressiveReductions3: Joi.string().required().messages({
-    'string.base': 'progressiveReductions3 should be a type of string',
-    'any.required': 'The field progressiveReductions3 is not present but it is required'
-  }),
-  progressiveReductions4: Joi.string().required().messages({
-    'string.base': 'progressiveReductions4 should be a type of string',
-    'any.required': 'The field progressiveReductions4 is not present but it is required'
-  }),
-  referenceAmount: Joi.string().required().messages({
-    'string.base': 'referenceAmount should be a type of string',
-    'any.required': 'The field referenceAmount is not present but it is required'
-  }),
-  totalProgressiveReduction: Joi.string().required().messages({
-    'string.base': 'totalProgressiveReduction should be a type of string',
-    'any.required': 'The field totalProgressiveReduction is not present but it is required'
-  }),
-  totalDelinkedPayment: Joi.string().required().messages({
-    'string.base': 'totalDelinkedPayment should be a type of string',
-    'any.required': 'The field totalDelinkedPayment is not present but it is required'
-  }),
-  paymentAmountCalculated: Joi.string().required().messages({
-    'string.base': 'paymentAmountCalculated should be a type of string',
-    'any.required': 'The field paymentAmountCalculated is not present but it is required'
-  }),
+  sbi: createNumberField('sbi', constants.minSbi, constants.maxSbi),
+  frn: createNumberField('frn', constants.minFrn, constants.maxFrn),
+  ...paymentBands,
+  ...percentageReductions,
+  ...progressiveReductions,
+  referenceAmount: createStringField('referenceAmount'),
+  totalProgressiveReduction: createStringField('totalProgressiveReduction'),
+  totalDelinkedPayment: createStringField('totalDelinkedPayment'),
+  paymentAmountCalculated: createStringField('paymentAmountCalculated'),
   datePublished: Joi.date().allow(null).messages({
     'date.base': 'datePublished should be a type of date',
     'date.strict': 'datePublished should be a type of date or null'
