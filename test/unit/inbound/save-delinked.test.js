aff1b02cfee680e6015ae96e7911202dc9afb607
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2024-08-07 09:25:56 +0100

    save-delinked.test.js

--- test/unit/inbound/save-delinked.test.js
+++ test/unit/inbound/save-delinked.test.js
@@ -31,4 +31,30 @@ describe('saveDelinked', () => {
 
     await expect(saveDelinked(delinkedCalculation, transaction)).rejects.toThrow(`Error saving total with Calculation Id calculationReference1: ${errorMessage}`)
   })
+
+  test('should transform delinkedCalculation correctly', async () => {
+    db.delinkedCalculation.create.mockResolvedValue()
+
+    await saveDelinked(delinkedCalculation, transaction)
+    const transformedDelinked = {
+      calculationId: 'calculationReference1',
+      applicationId: 'applicationReference1'
+    }
+
+    expect(db.delinkedCalculation.create).toHaveBeenCalledWith(transformedDelinked, { transaction })
+  })
+
+  test('should not contain calculationReference and applicationReference after transformation', async () => {
+    db.delinkedCalculation.create.mockResolvedValue()
+
+    await saveDelinked(delinkedCalculation, transaction)
+
+    const transformedDelinked = {
+      calculationId: 'calculationReference1',
+      applicationId: 'applicationReference1'
+    }
+
+    expect(transformedDelinked).not.toHaveProperty('calculationReference')
+    expect(transformedDelinked).not.toHaveProperty('applicationReference')
+  })
 })
