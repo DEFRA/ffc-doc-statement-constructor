735e0f8b1f24e6f9272aae304189ec0082954356
John Barnard <john.barnard.external@eviden.com>
2025-01-27 11:23:41 +0000

    throw error settlement received more than x hours ago

--- app/inbound/return/process-return-settlement.js
+++ app/inbound/return/process-return-settlement.js
@@ -2,6 +2,7 @@ const db = require('../../data')
 const saveSettlement = require('./save-settlement')
 const saveSchedule = require('./save-schedule')
 const getSettlementByInvoiceNumberAndValue = require('./get-settlement-by-invoice-number-and-value')
+const config = require('../../config').processingConfig
 
 const processReturnSettlement = async (settlement) => {
   const transaction = await db.sequelize.transaction()
@@ -10,6 +11,17 @@ const processReturnSettlement = async (settlement) => {
     const existingSettlement = await getSettlementByInvoiceNumberAndValue(settlement.invoiceNumber, settlement.value, transaction)
     if (existingSettlement) {
       console.info(`Duplicate settlement received, skipping ${existingSettlement.reference}`)
+
+      const hoursLimit = config.hoursLimit
+      const receivedDate = new Date(existingSettlement.received)
+      const currentDate = new Date()
+      const hoursDifference = Math.abs(currentDate - receivedDate) / 36e5
+
+      if (hoursDifference > hoursLimit) {
+        console.error(`Settlement ${existingSettlement.invoiceNumber} was received more than ${hoursLimit} hours ago.`)
+        throw new Error(`Settlement ${existingSettlement.invoiceNumber} was received more than ${hoursLimit} hours ago.`)
+      }
+
       await transaction.rollback()
     } else {
       const savedSettlement = await saveSettlement(settlement, transaction)
