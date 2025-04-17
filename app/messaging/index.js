67353c8e2070867298661b1945d4fe17e7e5210b
Oliver Lewington <oliver.lewington@eviden.com>
2025-04-17 09:32:39 +0100

    Removing relations to return, submit and processing Service Bus connections

--- app/messaging/index.js
+++ app/messaging/index.js
@@ -1,9 +1,7 @@
 const config = require('../config')
 const processStatementDataMessage = require('./process-statement-data-message')
 const { MessageReceiver } = require('ffc-messaging')
-let processingReceiver
-let submitReceiver
-let returnReceiver
+
 let statementDataReceiver
 
 const start = async () => {
@@ -15,9 +13,6 @@ const start = async () => {
 }
 
 const stop = async () => {
-  await processingReceiver.closeConnection()
-  await submitReceiver.closeConnection()
-  await returnReceiver.closeConnection()
   await statementDataReceiver.closeConnection()
 }
 
