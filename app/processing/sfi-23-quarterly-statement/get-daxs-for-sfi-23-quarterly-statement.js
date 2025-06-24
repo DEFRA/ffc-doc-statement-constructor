971527a713197a802b06ab9da475f4c6f4c84483
Sam Plackett <samuel.plackett@eviden.com>
2025-06-24 15:43:42 +0100

    do not process statements yet to be paid

--- app/processing/sfi-23-quarterly-statement/get-daxs-for-sfi-23-quarterly-statement.js
+++ app/processing/sfi-23-quarterly-statement/get-daxs-for-sfi-23-quarterly-statement.js
@@ -2,27 +2,38 @@ const db = require('../../data')
 const config = require('../../config').processingConfig
 
 const getDaxsForSfi23QuarterlyStatement = async (transaction) => {
-  return db.dax.findAll({
-    lock: true,
-    skipLocked: true,
-    order: [
-      ['lastProcessAttempt', 'ASC']
-    ],
-    limit: config.maxProcessingBatchSize,
-    transaction,
-    attributes: [
-      'daxId',
-      'calculationId',
-      'paymentReference',
-      'paymentPeriod',
-      'paymentAmount',
-      'transactionDate'
-    ],
-    where: {
-      startPublish: null
-    },
-    raw: true
-  })
+  try {
+    const today = new Date()
+    today.setHours(0, 0, 0, 0)
+
+    const daxRecords = await db.dax.findAll({
+      lock: true,
+      skipLocked: true,
+      order: [
+        ['lastProcessAttempt', 'ASC']
+      ],
+      limit: config.maxProcessingBatchSize,
+      transaction,
+      attributes: [
+        'daxId',
+        'calculationId',
+        'paymentReference',
+        'paymentPeriod',
+        'paymentAmount',
+        'transactionDate'
+      ],
+      where: {
+        startPublish: null,
+        transactionDate: { [db.Sequelize.Op.lt]: today }
+      },
+      raw: true
+    })
+    return daxRecords
+
+  } catch (error) {
+      console.error('Error fetching dax records for SFI23 statements:', error)
+      throw new Error('Failed to fetch dax records for SFI23 statements')
+  }
 }
 
 module.exports = getDaxsForSfi23QuarterlyStatement
