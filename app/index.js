d62e4410bd94c56366b895d00414ce2de2ea8bb9
Leigh Godson <leigh.godson@atos.net>
2025-09-26 17:17:30 +0100

    add alerting.init to pass data to lib

--- app/index.js
+++ app/index.js
@@ -1,6 +1,21 @@
 require('log-timestamp')
 require('./insights').setup()
 
+const alerting = require('ffc-alerting-utils')
+const messageConfig = require('./config/message')
+const { SOURCE } = require('./constants/source')
+const { DATA_PROCESSING_ERROR } = require('./constants/alerts')
+const { EventPublisher } = require('ffc-pay-event-publisher')
+
+if (alerting.init) {
+  alerting.init({
+    topic: messageConfig.alertTopic,
+    source: SOURCE,
+    defaultType: DATA_PROCESSING_ERROR,
+    EventPublisherClass: EventPublisher
+  })
+}
+
 const messaging = require('./messaging')
 const processing = require('./processing')
 
