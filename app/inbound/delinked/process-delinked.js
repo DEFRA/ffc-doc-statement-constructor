b26c2a141b0e01ae4e770146592de2c8f96dcc9e
Sam Plackett <60177449+samplackett@users.noreply.github.com>
2025-10-17 11:06:15 +0100

    add alerting in the event of duplicate data received from statement-data (#114)

--- app/inbound/delinked/process-delinked.js
+++ app/inbound/delinked/process-delinked.js
@@ -1,14 +1,22 @@
+const { dataProcessingAlert } = require('ffc-alerting-utils')
 const db = require('../../data')
 const savePlaceholderOrganisation = require('./save-placeholder-organisation')
 const getDelinkedByCalculationId = require('./get-delinked-by-calculation-id')
 const saveDelinked = require('./save-delinked')
 const validateDelinked = require('./validate-delinked')
+const { DUPLICATE_RECORD } = require('../../constants/alerts')
 
 const processDelinked = async (delinked) => {
   try {
     const existingDelinked = await getDelinkedByCalculationId(delinked.calculationReference)
     if (existingDelinked) {
       console.info(`Duplicate delinked received, skipping ${existingDelinked.calculationId}`)
+      await dataProcessingAlert({
+        process: 'processDelinked',
+        ...delinked,
+        message: `A duplicate record was received for calculation ID ${existingDelinked.calculationId}`,
+        type: DUPLICATE_RECORD
+      }, DUPLICATE_RECORD)
       return
     }
 
