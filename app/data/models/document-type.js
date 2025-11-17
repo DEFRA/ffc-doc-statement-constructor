10c30ab9c14f37651a446c022290dfdcc53fd2f4
John Barnard <john.barnard.external@eviden.com>
2025-11-17 15:55:46 +0000

    Added comments to document type fields
    Signed-off-by: John Barnard <john.barnard.external@eviden.com>

--- app/data/models/document-type.js
+++ app/data/models/document-type.js
@@ -1,9 +1,9 @@
 const documentTypeDB = (sequelize, DataTypes) => {
   const documentType = sequelize.define('documentType', {
-    documentTypeId: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    code: DataTypes.STRING,
-    name: DataTypes.STRING,
-    description: DataTypes.STRING
+    documentTypeId: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, comment: "Example Output: Source: Documents Used on Statement? No, used to pass configuration details to the CRM to allow integration between the two systems" },
+    code: { type: DataTypes.STRING, comment: "Example Output: Source: Documents Used on Statement? No, used to pass configuration details to the CRM to allow integration between the two systems"},
+    name: { type: DataTypes.STRING, comment: "Example Output: Source: Documents Used on Statement? No, used to pass configuration details to the CRM to allow integration between the two systems"},
+    description: { type: DataTypes.STRING, comment: "Example Output: Source: Documents Used on Statement? No, used to pass configuration details to the CRM to allow integration between the two systems"},
   },
   {
     tableName: 'documentTypes',
