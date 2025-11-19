8a159d6acca7bcbd56332c4420f1060e01b25698
John Barnard <john.barnard.external@eviden.com>
2025-11-19 12:23:18 +0000

    moved comments to constants
    Signed-off-by: John Barnard <john.barnard.external@eviden.com>

--- app/data/models/document-status.js
+++ app/data/models/document-status.js
@@ -1,20 +1,34 @@
+const comment = 'Example Output: Source: Documents Used on Statement? No, used to pass configuration details to the CRM to allow integration between the two systems'
+
 const documentStatusDB = (sequelize, DataTypes) => {
   const documentStatus = sequelize.define('document', {
-    documentId: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, comment: 'Example Output: Source: Documents Used on Statement? No, used to pass configuration details to the CRM to allow integration between the two systems' },
-    documentTypeId: { type: DataTypes.INTEGER, comment: 'Example Output: Source: Documents Used on Statement? No, used to pass configuration details to the CRM to allow integration between the two systems' },
-    documentSourceReference: { type: DataTypes.STRING, comment: 'Example Output: Source: Documents Used on Statement? No, used to pass configuration details to the CRM to allow integration between the two systems' }
-  },
-  {
+    documentId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      comment
+    },
+    documentTypeId: {
+      type: DataTypes.INTEGER,
+      comment
+    },
+    documentSourceReference: {
+      type: DataTypes.STRING,
+      comment
+    }
+  }, {
     tableName: 'documents',
     freezeTableName: true,
     timestamps: false
   })
+
   documentStatus.associate = function (models) {
     documentStatus.belongsTo(models.documentType, {
       foreignKey: 'documentTypeId',
       as: 'documentTypes'
     })
   }
+
   return documentStatus
 }
 
