4a3a251e7a8f2c50715b2f1ef25e8c61b5c2cebc
John Barnard <john.barnard.external@eviden.com>
2025-11-19 12:23:58 +0000

    moved comments to constants
    Signed-off-by: John Barnard <john.barnard.external@eviden.com>

--- app/data/models/document-type.js
+++ app/data/models/document-type.js
@@ -1,15 +1,31 @@
+const comment = 'Example Output: Source: Documents Used on Statement? No, used to pass configuration details to the CRM to allow integration between the two systems'
+
 const documentTypeDB = (sequelize, DataTypes) => {
   const documentType = sequelize.define('documentType', {
-    documentTypeId: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, comment: 'Example Output: Source: Documents Used on Statement? No, used to pass configuration details to the CRM to allow integration between the two systems' },
-    code: { type: DataTypes.STRING, comment: 'Example Output: Source: Documents Used on Statement? No, used to pass configuration details to the CRM to allow integration between the two systems' },
-    name: { type: DataTypes.STRING, comment: 'Example Output: Source: Documents Used on Statement? No, used to pass configuration details to the CRM to allow integration between the two systems' },
-    description: { type: DataTypes.STRING, comment: 'Example Output: Source: Documents Used on Statement? No, used to pass configuration details to the CRM to allow integration between the two systems' }
-  },
-  {
+    documentTypeId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      comment
+    },
+    code: {
+      type: DataTypes.STRING,
+      comment
+    },
+    name: {
+      type: DataTypes.STRING,
+      comment
+    },
+    description: {
+      type: DataTypes.STRING,
+      comment
+    }
+  }, {
     tableName: 'documentTypes',
     freezeTableName: true,
     timestamps: false
   })
+
   return documentType
 }
 
