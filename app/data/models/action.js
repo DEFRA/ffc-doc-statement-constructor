060090b12793dee9a42525cfab05c8c981e46ef2
John Barnard <john.barnard.external@eviden.com>
2025-11-19 08:50:06 +0000

    Moved comment into constant
    Signed-off-by: John Barnard <john.barnard.external@eviden.com>

--- app/data/models/action.js
+++ app/data/models/action.js
@@ -8,33 +8,37 @@ const actionDB = (sequelize, DataTypes) => {
   const number50 = 50
   const number100 = 100
 
+  const comment = 'To be removed. RPA have just confirmed that SFI-23 and SFI-EO statements will not be issued, which has now introduced some technical debt to remove this.'
+
   const action = sequelize.define('action', {
-    actionId: { type: DataTypes.INTEGER, primaryKey: true, comment: 'To be removed. RPA have just confirmed that SFI-23 and SFI-EO statements will not be issued, which has now introduced some technical debt to remove this.' },
-    calculationId: { type: DataTypes.INTEGER, comment: 'To be removed. RPA have just confirmed that SFI-23 and SFI-EO statements will not be issued, which has now introduced some technical debt to remove this.' },
-    fundingCode: { type: DataTypes.STRING(number5), comment: 'To be removed. RPA have just confirmed that SFI-23 and SFI-EO statements will not be issued, which has now introduced some technical debt to remove this.' },
-    groupName: { type: DataTypes.STRING(number100), comment: 'To be removed. RPA have just confirmed that SFI-23 and SFI-EO statements will not be issued, which has now introduced some technical debt to remove this.' },
-    actionCode: { type: DataTypes.STRING(number5), comment: 'To be removed. RPA have just confirmed that SFI-23 and SFI-EO statements will not be issued, which has now introduced some technical debt to remove this.' },
-    actionName: { type: DataTypes.STRING(number100), comment: 'To be removed. RPA have just confirmed that SFI-23 and SFI-EO statements will not be issued, which has now introduced some technical debt to remove this.' },
-    rate: { type: DataTypes.STRING(number50), comment: 'To be removed. RPA have just confirmed that SFI-23 and SFI-EO statements will not be issued, which has now introduced some technical debt to remove this.' },
-    landArea: { type: DataTypes.DECIMAL(number18, number6), comment: 'To be removed. RPA have just confirmed that SFI-23 and SFI-EO statements will not be issued, which has now introduced some technical debt to remove this.' },
-    uom: { type: DataTypes.STRING(number10), comment: 'To be removed. RPA have just confirmed that SFI-23 and SFI-EO statements will not be issued, which has now introduced some technical debt to remove this.' },
-    annualValue: { type: DataTypes.STRING(number50), comment: 'To be removed. RPA have just confirmed that SFI-23 and SFI-EO statements will not be issued, which has now introduced some technical debt to remove this.' },
-    quarterlyValue: { type: DataTypes.DECIMAL(number15, number2), comment: 'To be removed. RPA have just confirmed that SFI-23 and SFI-EO statements will not be issued, which has now introduced some technical debt to remove this.' },
-    overDeclarationPenalty: { type: DataTypes.DECIMAL(number15, number2), comment: 'To be removed. RPA have just confirmed that SFI-23 and SFI-EO statements will not be issued, which has now introduced some technical debt to remove this.' },
-    quarterlyPaymentAmount: { type: DataTypes.DECIMAL(number15, number2), comment: 'To be removed. RPA have just confirmed that SFI-23 and SFI-EO statements will not be issued, which has now introduced some technical debt to remove this.' },
-    datePublished: { type: DataTypes.DATE, allowNull: true, comment: 'To be removed. RPA have just confirmed that SFI-23 and SFI-EO statements will not be issued, which has now introduced some technical debt to remove this.' }
+    actionId: { type: DataTypes.INTEGER, primaryKey: true, comment },
+    calculationId: { type: DataTypes.INTEGER, comment },
+    fundingCode: { type: DataTypes.STRING(number5), comment },
+    groupName: { type: DataTypes.STRING(number100), comment },
+    actionCode: { type: DataTypes.STRING(number5), comment },
+    actionName: { type: DataTypes.STRING(number100), comment },
+    rate: { type: DataTypes.STRING(number50), comment },
+    landArea: { type: DataTypes.DECIMAL(number18, number6), comment },
+    uom: { type: DataTypes.STRING(number10), comment },
+    annualValue: { type: DataTypes.STRING(number50), comment },
+    quarterlyValue: { type: DataTypes.DECIMAL(number15, number2), comment },
+    overDeclarationPenalty: { type: DataTypes.DECIMAL(number15, number2), comment },
+    quarterlyPaymentAmount: { type: DataTypes.DECIMAL(number15, number2), comment },
+    datePublished: { type: DataTypes.DATE, allowNull: true, comment }
   },
   {
     tableName: 'actions',
     freezeTableName: true,
     timestamps: false
   })
+
   action.associate = function (models) {
     action.belongsTo(models.total, {
       foreignKey: 'calculationId',
       as: 'actions'
     })
   }
+
   return action
 }
 
--- app/data/models/document-status.js
+++ app/data/models/document-status.js
@@ -1,8 +1,8 @@
 const documentStatusDB = (sequelize, DataTypes) => {
   const documentStatus = sequelize.define('document', {
-    documentId: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, comment: "Example Output: Source: Documents Used on Statement? No, used to pass configuration details to the CRM to allow integration between the two systems" },
-    documentTypeId: { type: DataTypes.INTEGER, comment: "Example Output: Source: Documents Used on Statement? No, used to pass configuration details to the CRM to allow integration between the two systems"},
-    documentSourceReference: { type: DataTypes.STRING, comment: "Example Output: Source: Documents Used on Statement? No, used to pass configuration details to the CRM to allow integration between the two systems"}
+    documentId: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, comment: 'Example Output: Source: Documents Used on Statement? No, used to pass configuration details to the CRM to allow integration between the two systems' },
+    documentTypeId: { type: DataTypes.INTEGER, comment: 'Example Output: Source: Documents Used on Statement? No, used to pass configuration details to the CRM to allow integration between the two systems' },
+    documentSourceReference: { type: DataTypes.STRING, comment: 'Example Output: Source: Documents Used on Statement? No, used to pass configuration details to the CRM to allow integration between the two systems' }
   },
   {
     tableName: 'documents',
