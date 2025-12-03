9530678f3372593538016598d882dfc3b08f00f6
Oliver Lewington <oliver.lewington@atos.net>
2025-12-03 16:20:40 +0000

    Change wording of deletion log

--- app/inbound/organisation/check-and-remove-empty-address.js
+++ app/inbound/organisation/check-and-remove-empty-address.js
@@ -1,6 +1,7 @@
 const db = require('../../data')
 
 const checkAndRemoveEmptyAddress = async (organisation, transaction) => {
+  const logMessage = `Deleting organisation ${organisation.sbi}`
   const noAddress = !organisation.addressLine1 && !organisation.addressLine2 && !organisation.addressLine3 && !organisation.city && !organisation.county && !organisation.postcode
 
   if (!noAddress) {
@@ -13,13 +14,13 @@ const checkAndRemoveEmptyAddress = async (organisation, transaction) => {
   })
 
   if (existing) {
-    console.log(`Deleting organisation ${organisation.sbi} — no address provided`)
+    console.log(`${logMessage} — no address provided`)
     await db.organisation.destroy({
       where: { sbi: organisation.sbi },
       transaction
     })
   } else {
-    console.log(`Skipping save — organisation ${organisation.sbi} has no address and does not exist in DB`)
+    console.log(`${logMessage}  - no address provided and does not exist in DB`)
   }
 
   return true
