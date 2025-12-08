abdea91b561c57cf57b76f718d125a37180acc4d
Oliver Lewington <oliver.lewington@atos.net>
2025-12-08 10:52:36 +0000

    Adding functionality to remove inbound records with empty addresses (#123)
    * Adding functionality to remove inbound records with empty addresses
    
    * 2.0.30
    
    * Change wording of deletion log
    
    * Removing space
    
    * Formatting
    
    ---------
    
    Co-authored-by: ffcplatform <ffcplatforms@environment-agency.gov.uk>

--- app/inbound/organisation/process-organisation.js
+++ app/inbound/organisation/process-organisation.js
@@ -1,12 +1,19 @@
 const db = require('../../data')
 const saveOrganisation = require('./save-organisation')
 const validateOrganisation = require('./validate-organisation')
+const { checkAndRemoveEmptyAddress } = require('./check-and-remove-empty-address')
 
 const processOrganisation = async (organisation) => {
   const transaction = await db.sequelize.transaction()
+
   try {
-    validateOrganisation(organisation, organisation.sbi)
-    await saveOrganisation(organisation, transaction)
+    const removed = await checkAndRemoveEmptyAddress(organisation, transaction)
+
+    if (!removed) {
+      validateOrganisation(organisation, organisation.sbi)
+      await saveOrganisation(organisation, transaction)
+    }
+
     await transaction.commit()
   } catch (error) {
     await transaction.rollback()
