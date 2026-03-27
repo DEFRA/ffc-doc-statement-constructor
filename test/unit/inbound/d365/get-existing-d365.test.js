ce7790ef4ebe535c65168ff47bef95216d3f7ac1
Sam Plackett <samuel.plackett@eviden.com>
2026-03-27 16:35:17 +0000

    change duplicate detection for d365, remove duplicate unpublished d365

--- test/unit/inbound/d365/get-existing-d365.test.js
+++ test/unit/inbound/d365/get-existing-d365.test.js
@@ -1,5 +1,5 @@
 const db = require('../../../../app/data')
-const getd365ByCalculationIdAndPaymentReference = require('../../../../app/inbound/d365/get-d365-by-calculation-id-and-payment-reference')
+const getExistingD365 = require('../../../../app/inbound/d365/get-existing-d365')
 
 jest.mock('../../../../app/data', () => ({
   d365: {
@@ -7,8 +7,8 @@ jest.mock('../../../../app/data', () => ({
   }
 }))
 
-describe('getd365ByCalculationIdAndPaymentReference', () => {
-  const d365 = { calculationReference: 123, paymentReference: 'PY12345' }
+describe('getExistingD365', () => {
+  const d365 = { paymentReference: 'PY12345', paymentPeriod: 'Q4-2025', paymentAmount: '100.00', transactionDate: '2026-01-01' }
   const transaction = { id: 'txn1' }
 
   beforeEach(() => {
@@ -17,24 +17,24 @@ describe('getd365ByCalculationIdAndPaymentReference', () => {
 
   test('calls db.d365.findOne with correct arguments', async () => {
     db.d365.findOne.mockResolvedValue({})
-    await getd365ByCalculationIdAndPaymentReference(d365, transaction)
+    await getExistingD365(d365, transaction)
     expect(db.d365.findOne).toHaveBeenCalledWith({
       transaction,
       lock: true,
-      where: { calculationId: 123, paymentReference: 'PY12345' }
+      where: { paymentReference: 'PY12345', paymentPeriod: 'Q4-2025', paymentAmount: '100.00', transactionDate: '2026-01-01' }
     })
   })
 
   test('returns the result from db.d365.findOne', async () => {
     const expected = { id: 1, calculationId: 123, paymentReference: 'PY12345' }
     db.d365.findOne.mockResolvedValue(expected)
-    const result = await getd365ByCalculationIdAndPaymentReference(d365, transaction)
+    const result = await getExistingD365(d365, transaction)
     expect(result).toBe(expected)
   })
 
   test('returns null if db.d365.findOne returns null', async () => {
     db.d365.findOne.mockResolvedValue(null)
-    const result = await getd365ByCalculationIdAndPaymentReference(d365, transaction)
+    const result = await getExistingD365(d365, transaction)
     expect(result).toBeNull()
   })
 })
